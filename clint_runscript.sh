86a6217907e6a17748742e0cc22ce5cf53f3a2d0
Clint Torres <thisisclintsemail@gmail.com>
2021-07-10 01:32:17 -0700

    warning about mpmath needed edit to code

--- clint_runscript.sh
+++ clint_runscript.sh
@@ -5,6 +5,9 @@ python3 mandelbrot.py --gif=flint.gif --duration=.25 --color=exp2 --verbose=3 --
 #python3 mandelbrot.py --gif=decimal.gif --duration=.25 --color=exp2 --verbose=3 --burn --decimal --build-cache --cache-path='cache'
 #python3 mandelbrot.py --gif=decimal.gif --duration=.25 --color=exp2 --verbose=3 --burn --decimal --build-cache --invalidate-cache --cache-path='cache'
 
+#
+# NOTE: MUST change instantiation of MandlContext() at line ~810
+#
 #python3 mandelbrot.py --gif=mpmath.gif --duration=.25 --color=exp2 --verbose=3 --burn --build-cache --cache-path='cache'
 #python3 mandelbrot.py --gif=mpmath.gif --duration=.25 --color=exp2 --verbose=3 --burn --build-cache --invalidate-cache --cache-path='cache'
 
