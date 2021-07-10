8a89f78a757b9ce7a75f5357f6c3bff99b7dab68
Clint Torres <thisisclintsemail@gmail.com>
2021-07-10 01:19:37 -0700

    Adding exponential color to different command line options

--- clint_runscript.sh
+++ clint_runscript.sh
@@ -1,8 +1,9 @@
-python3 mandelbrot.py --gif=flint.gif --duration=.25 --color --verbose=3 --burn --flint --build-cache --cache-path='cache'
-#python3 mandelbrot.py --gif=flint.gif --duration=.25 --color --verbose=3 --burn --flint --build-cache --invalidate-cache --cache-path='cache'
 
-#python3 mandelbrot.py --gif=decimal.gif --duration=.25 --color --verbose=3 --burn --decimal --build-cache --cache-path='cache'
-#python3 mandelbrot.py --gif=decimal.gif --duration=.25 --color --verbose=3 --burn --decimal --build-cache --invalidate-cache --cache-path='cache'
+python3 mandelbrot.py --gif=flint.gif --duration=.25 --color=exp2 --verbose=3 --burn --flint --build-cache --cache-path='cache'
+#python3 mandelbrot.py --gif=flint.gif --duration=.25 --color=exp2 --verbose=3 --burn --flint --build-cache --invalidate-cache --cache-path='cache'
 
-#python3 mandelbrot.py --gif=mpmath.gif --duration=.25 --color --verbose=3 --burn --build-cache --cache-path='cache'
-#python3 mandelbrot.py --gif=mpmath.gif --duration=.25 --color --verbose=3 --burn --build-cache --invalidate-cache --cache-path='cache'
+#python3 mandelbrot.py --gif=decimal.gif --duration=.25 --color=exp2 --verbose=3 --burn --decimal --build-cache --cache-path='cache'
+#python3 mandelbrot.py --gif=decimal.gif --duration=.25 --color=exp2 --verbose=3 --burn --decimal --build-cache --invalidate-cache --cache-path='cache'
+
+#python3 mandelbrot.py --gif=mpmath.gif --duration=.25 --color=exp2 --verbose=3 --burn --build-cache --cache-path='cache'
+#python3 mandelbrot.py --gif=mpmath.gif --duration=.25 --color=exp2 --verbose=3 --burn --build-cache --invalidate-cache --cache-path='cache'
