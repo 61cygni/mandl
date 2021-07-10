be85e0a3e7e60e908ec53dbf95466a8ec3a6ca19
Clint Torres <thisisclintsemail@gmail.com>
2021-07-10 01:27:21 -0700

    native float example commands added

--- clint_runscript.sh
+++ clint_runscript.sh
@@ -7,3 +7,6 @@ python3 mandelbrot.py --gif=flint.gif --duration=.25 --color=exp2 --verbose=3 --
 
 #python3 mandelbrot.py --gif=mpmath.gif --duration=.25 --color=exp2 --verbose=3 --burn --build-cache --cache-path='cache'
 #python3 mandelbrot.py --gif=mpmath.gif --duration=.25 --color=exp2 --verbose=3 --burn --build-cache --invalidate-cache --cache-path='cache'
+
+#python3 mandelbrot.py --gif=native.gif --duration=.25 --color=exp2 --verbose=3 --burn --build-cache --cache-path='cache'
+#python3 mandelbrot.py --gif=native.gif --duration=.25 --color=exp2 --verbose=3 --burn --build-cache --invalidate-cache --cache-path='cache'
